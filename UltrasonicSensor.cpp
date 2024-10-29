e0ff4aff7999cd7bdd959dfa3390226c7a183a01
Tanish Parlapall <105727102+Tanishcompsci@users.noreply.github.com>
2024-10-29 11:25:26 -0400

    Create UltrasonicSensor.cpp

---
+++ UltrasonicSensor.cpp
@@ -0,0 +1,45 @@
+#include <Enes100.h>
+
+// Defines Ultrasonic pins
+int trig1=11;
+int echo1=10;
+
+//Defines Ultrasonic Variables 
+long timeInMicro1;
+long distanceInCm1;
+
+
+void setup() {
+
+    Serial.begin(9600);
+    //Declares Ultrasonic pins as Input/Output
+    pinMode(11, OUTPUT);
+    pinMode(10, INPUT);
+    
+    Serial.println("Start of program");
+}
+
+void loop() {
+
+
+    //Code to trigger the trigger pin 11
+    digitalWrite(trig1,LOW);
+    delayMicroseconds(2);
+    digitalWrite(trig1,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig1,LOW);
+
+    //Echo code to get inputs
+    timeInMicro1= pulseIn(echo1,HIGH);
+    //timeInMicro2= pulseIn(echo2,HIGH);
+
+    //Speed of Sound programming
+    distanceInCm1= timeInMicro1 * .034 / 2;
+    //distanceInCm2= timeInMicro2/ 29 /2;
+
+    Serial.println(distanceInCm1);
+    //Serial.println(distanceInCm2);
+    delay(100);
+
+
+}
