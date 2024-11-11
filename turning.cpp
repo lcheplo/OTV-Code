0ab6e4f27cd20776279b2e9deed631bdab24e51b
Tanish Parlapall <105727102+Tanishcompsci@users.noreply.github.com>
2024-11-11 15:31:41 -0500

    Create turning.cpp

---
+++ turning.cpp
@@ -0,0 +1,39 @@
+#include "Arduino.h"
+#include "Enes100.h"
+#include "Tank.h"
+
+
+
+void setup() {
+    Enes100.begin("Simulator", FIRE, 3, 8, 9);
+    Tank.begin();
+   
+    Enes100.println("Starting driving");
+}
+
+void loop() {
+   
+    while(Enes100.getTheta() <= 2.9){
+        Tank.setLeftMotorPWM(-75);
+        Tank.setRightMotorPWM(75);
+    }
+    Tank.setLeftMotorPWM(0);
+    Tank.setRightMotorPWM(0);
+   
+    delay(2000);
+   
+   
+   
+    while(Enes100.getTheta() <= -1.75){
+        Tank.setLeftMotorPWM(-75);
+        Tank.setRightMotorPWM(75);
+    }
+   
+    Tank.setLeftMotorPWM(0);
+    Tank.setRightMotorPWM(0);
+   
+    delay(10000);
+    Enes100.print("X = "); Enes100.println(Enes100.getX());
+    Enes100.print("Y = "); Enes100.println(Enes100.getY());
+    Enes100.print("Theta = "); Enes100.println(Enes100.getTheta());
+}
